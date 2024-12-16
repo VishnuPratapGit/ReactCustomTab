aa6a463b74b20c15aac115cc11f00d7859dada87
Vishnu Pratap Singh <vishnupratap898@gmail.com>
2024-12-16 21:18:58 +0530

    css update

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -9,7 +9,7 @@ const Links = ({ data, contextMenu }) => {
       href={data.linkUrl}
       target="_blank"
       rel="noopener noreferrer"
-      className="dark:text-white text-black dark:hover:text-white border-2 border-transparent flex justify-center items-center w-max gap-2 mr-2 flex-shrink-0 hover:border-neutral-600 p-1 px-2 rounded-lg transition-all duration-500"
+      className="dark:text-white text-black dark:hover:text-white border-2 border-transparent flex justify-center items-center w-max gap-2 mr-3 flex-shrink-0 hover:border-neutral-600 p-1 px-2 rounded-lg transition-all duration-500"
     >
       {data.photoUrl.length === 0 ? (
         <img className="h-6" src={getFaviconUrl} />
