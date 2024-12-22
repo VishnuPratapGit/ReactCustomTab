682e72c6df229c1eae339ac14954eaa3b481c4d8
Vishnu Pratap Singh <vishnupratap898@gmail.com>
2024-12-23 02:23:47 +0530

    remove boder

--- src/components/DropArea.jsx
+++ src/components/DropArea.jsx
@@ -29,10 +29,10 @@ const DropArea = ({ draggableId, setDraggableId, position, ...others }) => {
       onDragOver={(e) => e.preventDefault()}
       className={`${display ? "opacity-50" : ""} ${showDrop
         ? "w-28 rounded-lg scale-100"
-        : "opacity-0 w-8 scale-90"
-        } flex items-center justify-center font-serif origin-left flex-shrink-0 h-max p-1.5 px-2 transition-all duration-300 border-dashed border select-none text-neutral-300`}
+        : "opacity-0 w-8 scale-90 border-transparent"
+        } flex items-center justify-center origin-left flex-shrink-0 h-max p-1.5 px-2 transition-all font-quicksand duration-300 border-dashed border select-none text-neutral-300`}
     >
-      {position + 1}
+      +
     </div>
   );
 };
