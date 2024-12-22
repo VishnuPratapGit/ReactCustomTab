c7bc04ea9c5717ab1c87c0e73e19d0cb4def9eca
Vishnu Pratap Singh <vishnupratap898@gmail.com>
2024-12-23 02:28:57 +0530

    numbers

--- src/components/DropArea.jsx
+++ src/components/DropArea.jsx
@@ -30,9 +30,9 @@ const DropArea = ({ draggableId, setDraggableId, position, ...others }) => {
       className={`${display ? "opacity-50" : ""} ${showDrop
         ? "w-28 rounded-lg scale-100"
         : "opacity-0 w-8 scale-90 border-transparent"
-        } flex items-center justify-center origin-left flex-shrink-0 h-max p-1.5 px-2 transition-all font-quicksand duration-300 border-dashed border select-none text-neutral-300`}
+        } flex items-center justify-center origin-left flex-shrink-0 h-max p-1.5 px-2 transition-all font-quicksand duration-300 mx-1 border-dashed border select-none text-neutral-300`}
     >
-      +
+      {position + 1}
     </div>
   );
 };
